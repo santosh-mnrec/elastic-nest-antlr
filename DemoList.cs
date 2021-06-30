09245c63ef325fa9133e7474873fab43b5088864
Santosh Singh <santosh.ksingh03@gmail.com>
2021-06-30 12:22:05 +0800

    Added JSON

--- DemoList.cs
+++ DemoList.cs
@@ -2,11 +2,8 @@ using static DemoParser;
 using Elasticsearch.Net;
 using Antlr4.Runtime.Misc;
 using System.Collections.Generic;
-using System.Linq;
 
 using Nest;
-using System.IO;
-using System;
 
 namespace dslq
 {
@@ -19,7 +16,7 @@ namespace dslq
 
         public Stack<QueryContainer> Stack { get; set; } = new Stack<QueryContainer>();
         public ElasticLowLevelClient client = new ElasticLowLevelClient();
-         
+
         public DemoList()
         {
 
@@ -29,14 +26,14 @@ namespace dslq
 
 
             var attrName = c.propertyName.GetText();
-            var attrValue = c.propertyValue.GetText().Trim();
+            var attrValue = c.propertyValue.GetText().Trim('\"');
             var temrsQuery = new TermQuery();
-           
-           var query2 = Query<dynamic>
-				.Bool(b => b.Must(new QueryContainer[] {
-				Query<string>
-				.Terms(q => q.Field("sample")
-				.Terms(new string[] { "a", "b" }))}));
+
+            var query2 = Query<dynamic>
+                 .Bool(b => b.Must(new QueryContainer[] {
+                Query<string>
+                .Terms(q => q.Field(attrName)
+                .Terms(attrValue))}));
             Stack.Push(query2);
         }
 
@@ -49,52 +46,27 @@ namespace dslq
             var left = Stack.Pop();
             var right = Stack.Pop();
 
-               var query2 = Query<dynamic>
-				.Bool(b => b.Must(new[]{left,right}));
-                
-            Stack.Push(left);
-            Stack.Push(right);
+            var query2 = Query<dynamic>
+             .Bool(b => b.Must(new[] { left, right }));
+
+           // Stack.Push(left);
+           // Stack.Push(right);
             Stack.Push(query2);
-           
+
 
         }
 
         public override void ExitOrLogicalExp([NotNull] OrLogicalExpContext context)
         {
-             var left = Stack.Pop();
-             var right = Stack.Pop();
+            var left = Stack.Pop();
+            var right = Stack.Pop();
             var query2 = Query<dynamic>
-				.Bool(b => b.Should(new[]{left,right}));
-                  Stack.Push(left);
-            Stack.Push(right);
+                .Bool(b => b.Should(new[] { left, right }));
+            //Stack.Push(left);
+          //  Stack.Push(right);
             Stack.Push(query2);
 
         }
-
-
-        public override void ExitStart([NotNull] StartContext context)
-        {
-                var r=PrintQuery(GetClient(),Stack.Peek());
-        }
-        public static IElasticClient GetClient()
-        {
-            var connection = new InMemoryConnection();
-            var connectionPool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
-            var settings = new ConnectionSettings(connectionPool, connection);
-            return new ElasticClient(settings);
-        }
-
-        public static string PrintQuery(IElasticClient esClient, QueryContainer search)
-        {
-            using (var ms = new MemoryStream())
-            {
-                esClient.SourceSerializer.Serialize(search, ms);
-                return  System.Text.Encoding.UTF8.GetString(ms.ToArray());
-            }
-
-        }
-
-
 
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using Antlr4.Runtime;
 using Antlr4.Runtime.Tree;
+using Elasticsearch.Net;
+
+using Nest;
+using System.IO;
+using Newtonsoft.Json.Linq;
 
 namespace dslq
 {
@@ -8,7 +13,7 @@ namespace dslq
     {
         static void Main(string[] args)
         {
-            var q = "price=2000 OR model=\"hyundai\" AND (color=\"red\" OR color=\"blue\")";
+            var q = @"name=""santosh"" OR price=2000 OR model=""hyundai"" AND (color=""red"" OR color=""blue"")";
             var stream = new AntlrInputStream(q);
             var lexer = new DemoLexer(stream);
             var tokenStream = new CommonTokenStream(lexer);
@@ -19,6 +24,25 @@ namespace dslq
 
             ParseTreeWalker walker = new ParseTreeWalker();
             walker.Walk(listener, tree);
+            System.Console.WriteLine(PrintQuery(GetClient(),listener.Stack.Peek()));
+        }
+          public static IElasticClient GetClient()
+        {
+            var connection = new InMemoryConnection();
+            var connectionPool = new SingleNodeConnectionPool(new Uri("http://localhost:9200"));
+            var settings = new ConnectionSettings(connectionPool, connection);
+            return new ElasticClient(settings);
+        }
+
+        public static string PrintQuery(IElasticClient esClient, QueryContainer search)
+        {
+            using (var ms = new MemoryStream())
+            {
+                esClient.SourceSerializer.Serialize(search, ms);
+                var s=  System.Text.Encoding.UTF8.GetString(ms.ToArray());
+                return JObject.Parse(s).ToString();
+            }
+
         }
     }
 }
